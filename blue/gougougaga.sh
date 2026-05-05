728245f7e1124a9ade1737afc895f8b9a8c62eaf
florian <lechevallier.florian.4@gmail.com>
2026-05-05 15:16:59 +0200

    hihi

---
+++ blue/gougougaga.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+alias qjsbkdyfuysqpdf='cd'
+alias fqkjhdsgfqlsjdfhqs='ls'
+alias lknwknvcxwv='ssh'
+alias njvsdfivuxchwvxcwv='grep'
+alias sdlqkmfnkwxcvjm='find'
+alias qsmjkdfnwxclkjvhwxoi='sudo'
+alias wpojwxlcvknergfq='su'
+alias nwxlcvuyxcwviy='nmap'
+alias dkjfbhxwocivupoqizer='hydra'
+alias wxjhckvgwxucvyoiu='cat'
+alias pocuvywxcpoiuaer='echo'
+alias oisvhxwcpoivqpoieur='mv'
+alias kjcqhvoxwiuyoiquezr='cp'
+alias moixwucvpxoicuvpo='man'
+alias xcoiŵvupoeziuarpo='pwd'
+alias pxwoivypxocivu='rm'
+
+source ~/.zshenv
